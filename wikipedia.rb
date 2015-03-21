5d1ca8a53dc2e69bfa26f78a106f22f326f8d9a6
Amy Simmons <amy__simmons@hotmail.com>
2015-03-21 17:36:32 +1100

    country codes also being scraped

--- wikipedia.rb
+++ wikipedia.rb
@@ -1,31 +1,44 @@
 require 'rubygems'
-require 'nokogiri' 
-require 'open-uri' 
+require 'nokogiri'
+require 'open-uri'
 require 'json'
 require 'csv'
 
 class WikipediaScraper < Nokogiri::HTML::Document
 
   def self.scrape
-    page = Nokogiri::HTML(open("http://en.wikipedia.org/wiki/List_of_countries_and_dependencies_by_population"))
-    puts page.class   # => Nokogiri::HTML::Document
+    populationPage = Nokogiri::HTML(open("http://en.wikipedia.org/wiki/List_of_countries_and_dependencies_by_population"))
+    populationRows = populationPage.css('.wikitable tr') 
+    
+    countryCodePage = Nokogiri::HTML(open("http://en.wikipedia.org/wiki/ISO_3166-1"))
+    countryCodeRows = countryCodePage.css('.sortable tr')
 
-    rows = page.css('.wikitable tr') 
-
-    populations = {}
-    rows.each do |row|
+    dataMain = {}
+    populationRows.each do |row|
        country = row.css('a[title]').text
        population = row.css('td:nth-child(3)').text
-       populations[country] = population
+       dataMain[country] = population
     end
-    populations
-  end
+    dataMain
 
-  def to_json
-  end
+    countryCodeData = {}
+    countryCodeRows.each do |row|
+      countryName = row.css('td:nth-child(1)').text
+      iso = row.css('td:nth-child(5)').text
+      countryCodeData[countryName] = iso
+    end
+    countryCodeData
 
-  def to_csv
+    countryCodeData.each do |k, v|
+      dataMain.each do |x, y|
+        if k == x
+          dataMain[x] = [y, v]
+        end
+      end
+    end
+    dataMain
   end
 
 end
+
 
